451d63134073eb41adf9e3bacd682c45c63607ca
Alissa Riekötter <alissa.riekoetter@web.de>
2021-02-22 13:19:20 +0100

    tries to add functionality to cart-button - something is wrong

--- src/js/shop.js
+++ src/js/shop.js
@@ -1,18 +1,29 @@
 import products from "./products.json";
-console.log (products);
 
+let cart =[];
+
+function handleButtonClick(event) {
+    const {productId} = this.dataset;
+    //console.log(productID) irgendwas läuft hier schon nicht, weil die productId nicht definiert ist. Wieso nicht??
+    const chosenProduct = product.find((product) => product.id === productId);
+
+    cart.push();
+}
 
 
 function shop () {
     const productContainer = document.querySelector('.shop-container');
     const typeTemplates = `
     ${products.map((product) => `
-    <div class="wrapper_type">
-        <div class="img_coffee_bag"></div>
-        <div class="type_name">${product.productName}</div>
-        <div class="price">${product.price / 100}€</div>
-        <div class="icons"></div>
-        <div class="description">${product.description}</div>
+    <div class="box_shop">
+        <div class="shop_coffee_variety">
+            <div class="shop_coffee_variety_img"></div>
+            <div class="type_name">${product.productName}</div>
+            <div class="shop_coffee_variety_price">${product.price / 100 + "0"}€</div>
+            <div class="shop_coffee_variety_icons"></div>
+            <div class="description">${product.description}</div>
+            <button class="cart_button" data-product-id="${product.id}">In den Warenkorb</button>
+        </div>
     </div>
     `
     )
@@ -20,9 +31,23 @@ function shop () {
     `;
 
     productContainer.innerHTML = typeTemplates;
+
+    const cartButtons = document.querySelectorAll(".cart_button");
+    console.log(cartButtons)
+
+    cartButtons.forEach((cartButton) => 
+    cartButton.addEventListener("click", handleButtonClick)
+    );
 }
 
 export default shop;
 
 
 
+// <div class="wrapper_type">
+// <div class="img_coffee_bag"></div>
+// <div class="type_name">${product.productName}</div>
+// <div class="price">${product.price / 100}€</div>
+// <div class="icons"></div>
+// <div class="description">${product.description}</div>
+// </div>
