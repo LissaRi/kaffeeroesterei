486ab018d4c6355bb77d255522b543670ca060bf
Alissa Riekötter <alissa.riekoetter@web.de>
2021-02-28 17:09:48 +0100

    renames single_product to singleProduct, moves product.scss to pages-folder, binds product-details to product.html but shows nothing. adds link to product.html in index-footer. yarn error????

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import navigation from "./navigation";
 import shop from "./shop";
-import single_product from "./single_product";
+import singleProduct from "./singleProduct";
 
 navigation();
 console.log(window.location);
@@ -8,4 +8,4 @@ if (window.location.pathname === "/_shop/index.html") {
     shop();
 }
 
-single_product();
+// singleProduct();
