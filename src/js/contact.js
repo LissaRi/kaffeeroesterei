7a212e712eeb7da61f6a3d3d0e65e3a8c28c129d
Alissa Riekötter <alissa.riekoetter@web.de>
2021-03-11 13:56:34 +0100

    adds functionality to contact input fields and button

--- src/js/contact.js
+++ src/js/contact.js
@@ -3,17 +3,19 @@
 
 //Checkt die Email-Adresse
 const email = document.getElementById ("email");
+const nameField = document.getElementById ("name");
+const phoneInput = document.getElementById ("phone");
 
-email.addEventListener("change", function (event) {
-    console.log("change");
-    // if (email.validity.typeMismatch) {
-    //     email.setCustomValidity("Trag bitte eine gültige E-Mail-Adresse ein!");
-    // }
-    // else {
-    //     email.setCustomValidity("");
-    // }
-});
 
+function validEmail () {
+    if (!this.value.includes ("@")) {  
+          email.classList.add("invalid");
+        } else {
+            email.classList.remove("invalid");
+        }
+  }
+
+email.addEventListener("change", validEmail);
 
 
 //Checkt die Telefonnummer
@@ -26,13 +28,40 @@ const checkboxInput = document.getElementById("privacy")
 
 
 // Button soll submitten
-document.querySelector("button").addEventListener("click", function () {
-    if (privacy.checked === false) {
-        alert("Bitte stimme der Datenschutzerklärung zu!")
-    };
+document.querySelector("button").addEventListener("click", function (event) {
+    event.preventDefault ();
+    
+    if (checkboxInput.checked === false) {
+        alert("Du hast das Häkchen nicht gesetzt!")
+    }
 
-    if (email.validity.typeMismatch) {
-        alert("Trage bitte eine gültige Emai-Adresse ein")
-    };
+    else if (email.validity.typeMismatch) {
+        // alert("Trage bitte eine gültige Emai-Adresse ein")
+       
+        
+ 
+
+    }
+    
+    else if (email.value === "" || nameField.value === "" || phoneInput.value === "") {
+        alert ("Du musst deinen Namen, eine Email-Adresse und eine Telefonnummer eintragen. Diese Felder dürfen nicht leer bleiben!")
+    }
+
+
     console.log("Es wurde geklickt")
 });
+
+
+
+const emailWarning = document.getElementsByClassName ("email-wrapper");
+const test = function () {
+    console.log (emailWarning);
+ return `
+<div class="email-wrapper">
+    <p>Eine Email muss ein @ Zeichen haben und mit .com enden</p>
+</div>
+`
+};
+
+emailWarning.innerHTML = test;
+console.log (emailWarning.innerHTML);
