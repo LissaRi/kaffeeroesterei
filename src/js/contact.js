f305f33a79951c619eea0ca02266d36482362e50
Alissa Riekötter <alissa.riekoetter@web.de>
2021-03-13 11:00:44 +0100

    changed things back, now confused

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,35 +1,53 @@
 "use strict";
-import { validateEmail, validateNotEmpty } from "./form";
+// import { validateEmail, validateNotEmpty } from "./form";
 
 const email = document.getElementById("email");
 const nameField = document.getElementById("name");
 const phoneInput = document.getElementById("phone");
 const checkboxInput = document.getElementById("privacy");
 
+// ---------------------------------------------------------------------------------------------------------------------
 function validate() {
-  let status = {
-    error: false,
-    errorMessages: [],
-  };
-  const errorElement = document.querySelector(`#${this.id}-error`);
-
-  if (this.id === "email") {
-    status = [...status, validateEmail(this.value)];
-    status = [...status, validateNotEmpty(this.value)];
-  }
-
-  console.log(status);
-
-  if (status.error) {
-    this.classList.add("invalid");
-    errorElement.innerHTML = status.errorMsg;
-  } else {
-    this.classList.remove("invalid");
-    errorElement.innerHTML = "";
-  }
+  // let status = {
+  //   error: false,
+  //   errorMessages: [],
+  // };
+  // const errorElement = document.querySelector(`#${this.id}-error`);
+  // if (this.id === "email") {
+  //   status = [...status, validateEmail(this.value)];
+  //   status = [...status, validateNotEmpty(this.value)];
+  // }
+  // console.log(status);
+  // if (status.error) {
+  //   this.classList.add("invalid");
+  //   errorElement.innerHTML = status.errorMsg;
+  // } else {
+  //   this.classList.remove("invalid");
+  //   errorElement.innerHTML = "";
+  // }
 }
 
 email.addEventListener("keyup", validate);
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+//Email
+export function validateEmail(value) {
+  if (!value.includes("@")) {
+    return "Eine Email muss ein @ Zeichen haben und mit .com enden";
+  } else {
+    return null;
+  }
+}
+
+const emailWarning = document.getElementById("email-error");
+const test = function () {
+  return `
+      <p>Eine Emailadresse muss ein @ Zeichen, sowie einen . beinhalten!</p>
+    `;
+};
+
+emailWarning.innerHTML = test();
 
 //Checkt den Namen
 //Checkt die Telefonnummer
